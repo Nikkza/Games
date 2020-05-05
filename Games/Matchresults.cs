4871c22f29f5b24354750f88bfb0ea54e4490e83
Nikkza <isaksson.nicklas80@outlook.com>
2020-05-05 14:38:29 +0300

    Add project files.

---
+++ Games/Matchresults.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Games
+{
+    public class Matchresults
+    {
+        public int CountPoints { get; set; }
+        public int CountMatches { get; set; }
+        public int CountWinMatches { get; set; }
+        public int CountEvenMatches { get; set; }
+        public int CountLostMatches { get; set; }
+
+        public Matchresults Results(int _countPoints, int _countMatches,
+            int _countWinMatches, int _countEvenMatches, int _countLostMatches)
+        {
+            var getResultFromGame = new Matchresults()
+            {
+                CountPoints = _countPoints,
+                CountMatches = _countMatches,
+                CountWinMatches = _countWinMatches,
+                CountEvenMatches = _countEvenMatches,
+                CountLostMatches = _countLostMatches,
+
+            };
+            return getResultFromGame;
+        }
+        public List<string> CreatArray(string games)
+        {
+            Random random = new Random();
+            List<string> createMatchList = new List<string>();
+            var numberOfgames = long.Parse(games);
+
+            for (int i = 0; i < numberOfgames; i++)
+            {
+                var x = random.Next(0, 20);
+                var y = random.Next(0, 20);
+                createMatchList.Add(x.ToString() + ":" + y.ToString());
+            }
+
+            return createMatchList;
+        }
+
+        public Matchresults TotalAmountOfPoints(List<string> listOfgames, Matchresults result)
+        {
+            foreach (var item in listOfgames)
+            {
+                var splitByColonMatches = item.Split(':');
+                int[] convertToInArrayMatches = Array.ConvertAll(splitByColonMatches, int.Parse);
+                if (convertToInArrayMatches[0] > convertToInArrayMatches[1])
+                {
+                    result.CountPoints += 3;
+                    result.CountWinMatches++;
+                }
+                if (convertToInArrayMatches[0] < convertToInArrayMatches[1])
+                    result.CountLostMatches++;
+                if (convertToInArrayMatches[0] == convertToInArrayMatches[1])
+                {
+                    result.CountPoints += 1;
+                    result.CountEvenMatches++;
+                }
+                result.CountMatches++;
+            }
+
+            return result.Results(result.CountPoints, result.CountMatches, result.CountWinMatches, result.CountEvenMatches, result.CountLostMatches);
+        }
+
+        public string WriteOutResultsFromGame(Matchresults resultsFromGame)
+        {
+            return $"Total Games: {resultsFromGame.CountMatches}" + Environment.NewLine +
+            $"Points: {resultsFromGame.CountPoints}" + Environment.NewLine +
+            $"Wins: {resultsFromGame.CountWinMatches}" + Environment.NewLine +
+            $"Lost: {resultsFromGame.CountEvenMatches}" + Environment.NewLine +
+            $"Ewen: {resultsFromGame.CountLostMatches}";
+        }
+    }
+}
---
+++ Games/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Games
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number and press enter");
+            while (true)
+            {
+                Matchresults results = new Matchresults();
+                string test = Console.ReadLine();
+                var arrayCreated = results.CreatArray(test);
+                var resultsFromGame = results.TotalAmountOfPoints(arrayCreated, results);
+                Console.WriteLine(results.WriteOutResultsFromGame(resultsFromGame));
+
+            }
+        }
+    }
+}
